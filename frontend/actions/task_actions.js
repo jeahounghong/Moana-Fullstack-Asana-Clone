a66766247ec182bf0ddbdfbbb51f04e3f1a84101
“Jeahoung <jeahounghong@gmail.com>
2022-08-25 09:19:18 -0400

    new tasks automatically show sidebar

--- frontend/actions/task_actions.js
+++ frontend/actions/task_actions.js
@@ -2,6 +2,7 @@ import * as TaskApiUtil from "../util/task_api_util";
 export const RECEIVE_TASKS = "RECEIVE_TASKS";
 export const RECEIVE_TASK = "RECEIVE_TASK";
 export const REMOVE_TASK = "REMOVE_TASK";
+import { showUpdateTaskForm } from "./ui_actions";
 
 const receiveTask = (task) => ({
     type: RECEIVE_TASK,
@@ -31,7 +32,7 @@ export const fetchTask = (taskId) => dispatch => TaskApiUtil.fetchTask(taskId)
     .then(task => dispatch(receiveTask(task)))
 
 export const createTask = (task) => dispatch => TaskApiUtil.createTask(task)
-    .then(task => dispatch(receiveTask(task)));
+    .then(task => {dispatch(receiveTask(task)); dispatch(showUpdateTaskForm(task))} );
 
 export const updateTask = (task) => dispatch => TaskApiUtil.updateTask(task)
     .then(task => dispatch(receiveTask(task)));
