04e68419361758af6df24ba1ed01ec5993cd9bb7
“Jeahoung <jeahounghong@gmail.com>
2022-08-26 09:45:15 -0400

    spelling error

--- frontend/components/tasks/date.jsx
+++ frontend/components/tasks/date.jsx
@@ -48,7 +48,7 @@ export default class DateComponent extends React.Component {
             delete currentTask.ownerType;
             currentTask.user_id = currentTask.userId;
             delete currentTask.userId;
-            currentTask.title = this.currentTask.title;
+            currentTask.title = currentTask.title;
             // debugger;
             this.props.updateTask(currentTask);
 
